5821030b9b343b6f2ffa5c3d5c78c3d00855306e
ushwlwvw <126856143+ushwlwvw@users.noreply.github.com>
2023-04-05 20:43:51 +0300

    Update ping.py

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -46,21 +46,21 @@ async def ping_fallen(_, message: Message):
     uptime = get_readable_time((upt))
 
     await hmm.edit_text(
-        f"""⎊ آلبنج : `{resp}ᴍs`
+        f"""⌔︙ آلبنج : `{resp}ᴍs`
 
 <b><u>{BOT_NAME} آلحآله :</u></b>
 
-⎊ **مدة التشغيل :** {uptime}
-⎊ **الرام :** {mem}
-⎊ **وحدة المعالجة المركزية :** {cpu}
-⎊ **القرص :** {disk}""",
+⌔︙ **مدة التشغيل :** {uptime}
+⌔︙ **الرام :** {mem}
+⌔︙ **وحدة المعالجة المركزية :** {cpu}
+⌔︙ **القرص :** {disk}""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton("الدعم", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
-                        "السوࢪس",
-                        url="https://t.me/EE_20",
+                        "• 𝗖𝗔𝗟𝗜𝗣𝗛 ›",
+                        url="https://t.me/aaaalqp",
                     ),
                 ],
             ]
